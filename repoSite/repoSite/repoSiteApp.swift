e7f15530f40624edcaddb1c609ecaca1a191b4bf
SIVA3161 <sivaguru3161@gmail.com>
2021-07-02 08:39:34 +0530

    repoSite Swift project Init

---
+++ repoSite/repoSite/repoSiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  repoSiteApp.swift
+//  repoSite
+//
+//  Created by user176911 on 7/2/21.
+//
+
+import SwiftUI
+
+@main
+struct repoSiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
